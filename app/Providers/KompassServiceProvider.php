dec0c86210b4f9bc4ec0089bacfdb7c5b43e1cdf
Andreas Farah <andreasfarah@gmail.com>
2023-05-22 21:12:19 +0200

    fix: gate define

--- app/Providers/KompassServiceProvider.php
+++ app/Providers/KompassServiceProvider.php
@@ -51,5 +51,23 @@ class KompassServiceProvider extends ServiceProvider
         Fortify::twoFactorChallengeView(function () {
             return view('auth.two-factor-challenge');
         });
+
+        $roles = Role::with('users')->get();
+
+        $permissionsArray = [];
+        foreach ($roles as $role) {
+            $permissionsArray[$role->slug][] = $role->id;
+            // foreach ($role->slug as $permissions) {
+            //     $permissionsArray[$permissions->title][] = $role->id;
+            // }
+        }
+
+        // Every permission may have multiple roles assigned
+        foreach ($permissionsArray as $title => $roles) {
+            Gate::define($title, function ($user) use ($roles) {
+                // We check if we have the needed roles among current user's roles
+                return count(array_intersect($user->roles->pluck('id')->toArray(), $roles)) > 0;
+            });
+        }
     }
 }
