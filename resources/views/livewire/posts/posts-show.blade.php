73c205d38d04273a0f25bf40e8ec1c781014f2be
Andreas Farah <andreasfarah@gmail.com>
2023-11-15 12:20:34 +0100

    fix: button style

--- resources/views/livewire/posts/posts-show.blade.php
+++ resources/views/livewire/posts/posts-show.blade.php
@@ -6,7 +6,13 @@
             <x-slot name="button">
                 <button class="flex btn gap-x-2 justify-end items-center text-md"
                     wire:click="update('{{ $post->id }}')">
-                    <x-tabler-device-floppy class="icon-lg" />
+                    <div wire:loading>
+                        <svg class="animate-spin h-5 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                    </div>
+                    <x-tabler-device-floppy class="icon-lg" wire:loading.remove />
                     {{ __('Save') }}
                 </button>
             </x-slot>
@@ -163,7 +169,13 @@
 
                 <button class="flex btn gap-x-2 justify-end items-center text-md"
                     wire:click="update('{{ $post->id }}')">
-                    <x-tabler-device-floppy class="icon-lg" />
+                    <div wire:loading>
+                        <svg class="animate-spin h-5 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                    </div>
+                    <x-tabler-device-floppy class="icon-lg" wire:loading.remove />
                     {{ __('Save') }}
                 </button>
 
--- resources/views/livewire/posts/posts-table.blade.php
+++ resources/views/livewire/posts/posts-table.blade.php
@@ -9,7 +9,16 @@
                 <x-kompass::form.textarea wire:model="meta_description" id="name" name="Description"
                     label="Description" type="text" class="mt-1 block w-full h-[15rem]" />
 
-                <button wire:click="addPost" class="btn btn-primary">{{ __('Save') }}</button>
+                <button wire:click="addPost" class="btn btn-primary">
+                    <div wire:loading>
+                        <svg class="animate-spin h-5 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                    </div>
+                    <x-tabler-device-floppy class="icon-lg" wire:loading.remove />
+                    {{ __('Save') }}    
+                </button>
 
             </x-slot>
         </x-kompass::offcanvas>
