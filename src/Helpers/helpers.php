91c35fd6eade34d5b0f315bd527a54f26314b67f
secondnetwork <secondnetwork@users.noreply.github.com>
2024-01-14 17:07:33 +0000

    refactor: Laravel Pint changes

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -204,7 +204,7 @@ if (! function_exists('setting')) {
 
                 return $data->data;
             }
-        // return Arr::get(config('settings'), $data );
+            // return Arr::get(config('settings'), $data );
         } else {
             $data = Arr::get(app('settings'), $data);
 
