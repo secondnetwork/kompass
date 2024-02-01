990feebb8adfe91b209bdefa1e67759d15d1e76b
Andreas Farah <andreasfarah@gmail.com>
2024-02-01 16:36:35 +0100

    fix: kompassJs and kompassCss

--- src/BladeDirectives.php
+++ src/BladeDirectives.php
@@ -8,22 +8,26 @@ class BladeDirectives
 {
     public static function kompassCss()
     {
-        $url = 'http://localhost:5088:'.env('VITE_PORT_KOMPASS', '5088').'/resources/js/main.js';
+        $url = 'http://localhost:5088:' . env('VITE_PORT_KOMPASS', '5088') . '/resources/js/main.js';
         $url200 = @get_headers($url);
-        if (! $url200) {
-            $content = file_get_contents(asset('vendor/kompass/assets/manifest.json'));
+        if (!$url200) {
+            try {
+                $content = file_get_contents(asset('vendor/kompass/assets/manifest.json'));
+            } catch (\Throwable $th) {
+                $content = '';
+            }
+            // $content = file_get_contents(asset('vendor/kompass/assets/manifest.json'));
             $manifest = json_decode($content, true);
 
             $entry = 'resources/js/main.js';
 
-            if (! empty($manifest[$entry]['css'])) {
+            if (!empty($manifest[$entry]['css'])) {
                 foreach ($manifest[$entry]['css'] as $file) {
                     $urls[] = $file;
                     // $tags .= "<link rel=\"stylesheet\" href=\"$url\">";
                 }
+                return '<link rel="stylesheet" href="' . asset('vendor/kompass/assets/' . $urls[0]) . '"><style>[x-cloak] { display: none !important; }</style>';
             }
-
-            return '<link rel="stylesheet" href="'.asset('vendor/kompass/assets/'.$urls[0]).'"><style>[x-cloak] { display: none !important; }</style>';
         } else {
             return '';
         }
@@ -31,16 +35,22 @@ class BladeDirectives
 
     public static function kompassJs()
     {
-        $url = 'http://localhost:'.env('VITE_PORT_KOMPASS', '5088').'/resources/js/main.js';
+        $url = 'http://localhost:' . env('VITE_PORT_KOMPASS', '5088') . '/resources/js/main.js';
         $url200 = @get_headers($url);
-        if (! $url200) {
-            $content = file_get_contents(asset('vendor/kompass/assets/manifest.json'));
+        if (!$url200) {
+            try {
+                $content = file_get_contents(asset('vendor/kompass/assets/manifest.json'));
+            } catch (\Throwable $th) {
+                $content = '';
+            }
             $manifest = json_decode($content, true);
             $entry = 'resources/js/main.js';
-
-            return '<script type="module" defer src="'.asset('vendor/kompass/assets/'.$manifest[$entry]['file']).'"></script>';
+            if ($content) {
+                return '<script type="module" defer src="' . asset('vendor/kompass/assets/' . $manifest[$entry]['file']) . '"></script>';
+            }
+            
         } else {
-            return '<script type="module" defer src="'.$url.'"></script>';
+            return '<script type="module" defer src="' . $url . '"></script>';
         }
     }
 
