6b930f21b6c58e868fd0d37c1446087c8e05e628
Andreas Farah <andreasfarah@gmail.com>
2024-03-15 10:18:00 +0100

    fix: psr-4 Language.class

--- src/Http/Middleware/Language.php
+++ src/Http/Middleware/Language.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\URL;
 
-class language
+class Language
 {
     /**
      * Handle an incoming request.
