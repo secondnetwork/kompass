051e281e8c4fa4f0b184bd553a90d62938f77f55
Andreas Farah <andreasfarah@gmail.com>
2024-03-17 19:58:29 +0100

    fix: url storage

--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -337,7 +337,7 @@
                             <div class="relative">
 
                                 <img on="pages.pages-show" alt="logo" class="aspect-[4/3] w-full object-cover rounded-xl"
-                                    src="{{ asset('storage' . $file->path . '/' . $file->slug . '.' . $file->extension) }}">
+                                    src="{{ asset('storage/' . $file->path . '/' . $file->slug . '.' . $file->extension) }}">
                                 <action-button
                                     class="absolute flex justify-between items-center w-full bottom-0 right-0 z-10 p-3 gap-1 bg-gray-100/80 ">
                                     <div class="text-xs font-semibold truncate">{{ $file->name }}</div>
