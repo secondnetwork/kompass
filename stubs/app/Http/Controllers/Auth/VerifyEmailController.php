d7ca8c9b4bd98abb3c149d342620fef52704fd6e
Andreas Farah <andreasfarah@gmail.com>
2025-03-15 12:23:44 +0100

    fix: class name

--- stubs/app/Http/Controllers/Auth/VerifyEmailController.php
+++ stubs/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -7,7 +7,7 @@ use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
 
-class alt_VerifyEmailController extends Controller
+class VerifyEmailController extends Controller
 {
     /**
      * Mark the authenticated user's email address as verified.
