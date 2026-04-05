53105a2c50e7aaa4cd251435ffad25f78a9575f2
Andreas Farah <andreasfarah@gmail.com>
2026-04-05 17:59:52 +0200

    refactor: update typography styles for improved responsiveness and add alignment handling in group component

--- stubs/resources/views/components/blocks/group.blade.php
+++ stubs/resources/views/components/blocks/group.blade.php
@@ -7,9 +7,14 @@
         $layoutgrid = $item->layoutgrid ?? 12;
         $colSpan = $item->layoutgrid ? 'md:col-span-' . $item->layoutgrid : '';
         $gridCols = 'md:grid-cols-' . $layoutgrid;
+        $order = $item->getMeta('order') ?? '';
+        $orderClasses = match($order) {
+            'reverse' => 'flex flex-col-reverse',
+            default => '',
+        };
+        $align = $item->getMeta('align') ?? '';
     @endphp
-
-    <div class="group md:grid gap-6 transition-all ease-in-out duration-500 {{ $gridCols }} {{ $colSpan }} {{ get_meta($item, 'css-classname', '') }} {{ get_meta($item, 'layout', '') }} {{ get_meta($item, 'alignment', '') }}">
+    <div {{ $attributes->merge(['class' => 'group gap-6 md:grid ' . $align . ' ' . $orderClasses . ' ' . $gridCols . ' ' . $colSpan . ' ' . get_meta($item, 'css-classname', '') . ' ' . get_meta($item, 'layout', '') . ' ' . get_meta($item, 'alignment', '') ]) }}>
         @foreach ($item->children as $child)
             <x-blocks.components :item="$child" />
         @endforeach
