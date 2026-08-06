d47ea00d7ce2572dee69107586e9238e99f96c6a
Andreas Farah <andreasfarah@gmail.com>
2026-08-06 10:41:37 +0200

    fix: add theme synchronization on livewire navigation for improved user experience

--- stubs/resources/js/main.js
+++ stubs/resources/js/main.js
@@ -1,3 +1,9 @@
 
 import './plugins/lite-yt-embed'
 import './plugins/lite-vimeo-embed'
+
+function syncTheme() {
+    document.documentElement.setAttribute('data-theme', localStorage.getItem('theme') === 'dark' ? 'dark' : 'light')
+}
+
+document.addEventListener('livewire:navigated', syncTheme)
