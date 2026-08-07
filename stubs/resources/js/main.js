812d63b4d14b734a00f2060cd63b61d5e01461af
Andreas Farah <andreasfarah@gmail.com>
2026-08-07 14:17:14 +0200

    refactor: reorganize CSS files and enhance theme handling with new header height sync functionality

--- stubs/resources/js/main.js
+++ stubs/resources/js/main.js
@@ -7,3 +7,22 @@ function syncTheme() {
 }
 
 document.addEventListener('livewire:navigated', syncTheme)
+
+function syncHeaderHeight() {
+    const header = document.querySelector('header.navbar')
+    if (header) {
+        document.documentElement.style.setProperty('--header-h', header.offsetHeight + 'px')
+    }
+}
+
+function initHeaderOffset() {
+    syncHeaderHeight()
+
+    if (location.hash) {
+        document.querySelector(location.hash)?.scrollIntoView()
+    }
+}
+
+window.addEventListener('load', initHeaderOffset)
+window.addEventListener('resize', syncHeaderHeight)
+document.addEventListener('livewire:navigated', syncHeaderHeight)
